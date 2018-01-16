e8aa148ae2bd375f63f44ae4525985818a6741bb
Msatkin <mscottaa@gmail.com>
2018-01-16 11:37:18 +0100

    Implemented csv file reading

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import entities from './data/entities.csv';
+import CSVReader from 'react-csv-reader';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {file: null};
+  }
+
   render() {
-    console.log(entities);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Clarifai Test!</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <CSVReader
+          label="Select CSV with Image Urls  "
+          onFileLoaded={this.handleFileChange.bind(this)}
+          onError={this.handleFileError.bind(this)}
+        />
       </div>
     );
+  }
+
+  handleFileChange(file) {
+    console.log(file)
+    this.setState({file: file});
+  }
+
+  handleFileError(error) {
+    console.log(error);
   }
 }
 
